39397fda7a8b5c50892fb18504a9ab1cee1107a2
Syed Kamran <syed.kamran@mail.utoronto.ca>
2016-10-21 14:21:27 -0400

    adding return instead of echo

--- index.php
+++ index.php
@@ -6,14 +6,15 @@ require_once 'classes/Room.php';
 require_once 'classes/etc/limonade.php';
 
 dispatch_get('/:building/:room/:date', 'getSchedule');
-function getSchedule(){
-  $building = strtoupper(params('building'));
-  $room = params('room');
-  $date = params('date');
-  $room = new Room($building, $room, $date);
+function getSchedule()
+{
+    $building = strtoupper(params('building'));
+    $room = params('room');
+    $date = params('date');
+    $room = new Room($building, $room, $date);
 
-  $schedule = $room->getSchedule();
+    $schedule = $room->getSchedule();
 
-  echo $schedule;
+    return $schedule;
 }
 run();
